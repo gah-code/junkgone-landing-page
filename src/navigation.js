a25c11f82ee071515699c1e9cb01759ac0b5539a
Gilbert Haro <gilbertharocode@gmail.com>
2024-06-07 18:34:28 -0700

    social links and data for Footer

--- src/navigation.js
+++ src/navigation.js
@@ -133,13 +133,16 @@ export const footerData = {
       ],
     },
     {
-      title: 'Platform',
+      title: 'Locations',
       links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
+        { text: 'San Jose', href: '#' },
+        { text: 'Morgan Hill', href: '#' },
+        { text: 'Sunnyvale', href: '#' },
+        { text: 'Hollister', href: '#' },
+        { text: 'Santa Clara', href: '#' },
+        { text: 'Mountain View', href: '#' },
+        { text: 'Palo Alto', href: '#' },
+        { text: 'San Martin', href: '#' },
       ],
     },
     {
@@ -170,14 +173,25 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Tictok', icon: 'tabler:brand-tiktok', href: 'https://www.tiktok.com/@junk_gone21' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/junk_gone/' },
+    {
+      ariaLabel: 'Youtube',
+      icon: 'tabler:brand-linkedin',
+      href: 'https://www.youtube.com/@JunkGone408',
+    },
+    {
+      ariaLabel: 'Youtube',
+      icon: 'tabler:brand-youtube',
+      href: 'https://www.youtube.com/@JunkGone408',
+    },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/JGLLC46' },
+
+    // { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/gah-code/junkgone-landing-page' },
   ],
-  footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
-  `,
+  // footNote: `
+  //   <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
+  //   Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+  // `,
 };
